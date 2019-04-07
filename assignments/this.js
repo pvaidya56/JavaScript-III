76d74c54e1f6dafce34afacb0ad5fe39e3a113e3
pvaidya56 <pvaidya56@gmail.com>
2019-04-07 02:19:10 -0700

    Finished part 1

--- assignments/this.js
+++ assignments/this.js
@@ -1,11 +1,21 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
-*
+* 1. Window Binding 
+    This represents the window. It is basically all of the JS objects, functions and variables.
+    Logging this will output the whole window.
+
+* 2. Implicit Binding
+    This has a function within the object. An easy way to remember this is 'this' gets the value of whatever is left of the last dot in the function innvocation.
+
+* 3. Explicit Binding
+    Explicit binding is explicitly calling the function using the call, apply, and bind functions.
+    In these, we basically are telling 'this' what to be.
+
+* 4. New Binding
+*   This is using the 'new' keyword to create an object from a constructor. 
+    Constructor methods are preferred for reusability.
+    
 * write out a code example of each explanation above
 */
 
